f50818e7a266a07b850739f3bc1f8e7def23ba2e
igorkrosh <croshkin.igor@yandex.ru>
2025-09-19 10:13:33 +0300

    Added solution for task1 of basics section

---
+++ 01_basics/task1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integer int
+	var str string
+	var flag bool
+	var double float64
+
+	integer = 10
+	str = "abc"
+	flag = true
+	double = 3.14
+
+	fmt.Println("Int: ", integer)
+	fmt.Println("String: ", str)
+	fmt.Println("Bool: ", flag)
+	fmt.Println("Float64: ", double)
+}
